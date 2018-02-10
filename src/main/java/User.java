0c9f80fb3895272e657ff608bba79953bf9e21dc
shusrushabezugam <sbezugam@villanova.edu>
2018-02-10 02:57:17 -0500

    format and check style

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,46 +1,53 @@
 package main.java;
 
 public class User {
-String login;
-int id;
-public User()
-{
-	
-}
-public String getLogin() {
-	return login;
-}
-public void setLogin(String login) {
-	this.login = login;
-}
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-@Override
-public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + id;
-	return result;
-}
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	User other = (User) obj;
-	if (id != other.id)
-		return false;
-	return true;
-}
-@Override
-public String toString() {
-	return "User [login=" + login + ", id=" + id + "]";
-}
+    String login;
+    int id;
+
+    public User() {
+
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "User [login=" + login + ", id=" + id + "]";
+    }
 }
