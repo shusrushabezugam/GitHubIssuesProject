05cb960373cb560434bc17404d001f6996ca3770
shusrushabezugam <sbezugam@villanova.edu>
2018-02-09 22:51:47 -0500

    hashCode() and equals() added

--- src/main/java/Issue.java
+++ src/main/java/Issue.java
@@ -1,5 +1,28 @@
 package main.java;
 
-public class Issue {
+import java.util.Date;
 
+public class Issue {
+long number;
+int id;
+String state;
+String title;
+String body;
+Date createdAt;
+Date closedAt;
+User user;
+User assignee;
+public Issue(long number, int id, String state, String title, String body, Date createdAt, Date closedAt, User user,
+		User assignee) {
+	super();
+	this.number = number;
+	this.id = id;
+	this.state = state;
+	this.title = title;
+	this.body = body;
+	this.createdAt = createdAt;
+	this.closedAt = closedAt;
+	this.user = user;
+	this.assignee = assignee;
+}
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,5 +1,11 @@
 package main.java;
 
 public class User {
-
+String login;
+int id;
+public User(String login, int id) {
+	super();
+	this.login = login;
+	this.id = id;
+}
 }
