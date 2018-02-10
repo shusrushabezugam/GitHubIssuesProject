dbdf4a13959b37b43dc86465b340e9b92aee4db0
shusrushabezugam <sbezugam@villanova.edu>
2018-02-09 22:56:12 -0500

    toString() added constructor added

--- src/main/java/Issue.java
+++ src/main/java/Issue.java
@@ -25,4 +25,25 @@ public Issue(long number, int id, String state, String title, String body, Date 
 	this.user = user;
 	this.assignee = assignee;
 }
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + id;
+	return result;
+}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Issue other = (Issue) obj;
+	if (id != other.id)
+		return false;
+	return true;
+}
+
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -8,4 +8,24 @@ public User(String login, int id) {
 	this.login = login;
 	this.id = id;
 }
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + id;
+	return result;
+}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	User other = (User) obj;
+	if (id != other.id)
+		return false;
+	return true;
+}
 }
