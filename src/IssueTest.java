90d52498a4d5fe2ca2b4920b2eb6477500be7a54
shusrushabezugam <sbezugam@villanova.edu>
2018-02-14 17:36:50 -0500

    issue test

--- src/IssueTest.java
+++ src/IssueTest.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import main.java.Issue;
@@ -10,23 +13,57 @@ public class IssueTest {
     Issue i2=new Issue();
     User u1 = new User();
     User u2 = new User();
-    @Test
-    public void equalsTest() {
-      
+   
+   @Before
+   public void setUp()
+   {
        i1.setId(10);
+       i1.setAssignee(u1);
+       i1.setBody("Bug to be solved");
+       i1.setClosedAt(new Date());
+       i1.setAssignee(u1);
+       i1.setCreatedAt(new Date());
+       i1.setNumber(123);
+       i1.setTitle("First Bug");
+       i1.setState("open");
        i2.setId(20);
        u1.setId(10);
+       u1.setLogin("shusrushabezugam");
        u2.setId(10);
+       u2.setLogin("Ms-O");
+       
+       
+   }
+   
+    @Test
+    public void equalsIssueTest() {
+      
        assertTrue(i1.getId()==i2.getId());
            
     }
     @Test
-    public void hashCodeTest()
+    public void issueHashCodeTest()
     {
         int y,z;
         y=i1.hashCode();
         z=i2.hashCode();
         assertTrue(y==z);
     }
+    
+    @Test
+    public void userHashCodeTest()
+    {
+        int h1,h2;
+        h1=u1.hashCode();
+        h2=u2.hashCode();
+        assertTrue(h1==h2);
     }
-
+    @Test
+    public void equalsUserTest() {
+      
+       assertTrue(u1.getId()==u2.getId());
+           
+    }
+    
+    
+}
