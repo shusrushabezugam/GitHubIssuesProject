7f500482aad8e74bddbd461378658b02276366e9
shusrushabezugam <sbezugam@villanova.edu>
2018-03-14 15:47:02 -0400

    deleted

--- src/main/java/GitHubRestClient.java
+++
@@ -1,9 +0,0 @@
-package main.java;
-
-public class GitHubRestClient {
-
-    public String requestIssues(String username,String password) {
-        String jsonresponse=" ";
-        return jsonresponse;
-    }
-}
