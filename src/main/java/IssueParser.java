b88b09d99cb35df250f5ec801ad7f8729dcf176f
shusrushabezugam <sbezugam@villanova.edu>
2018-03-14 16:13:26 -0400

    date format

--- src/main/java/IssueParser.java
+++ src/main/java/IssueParser.java
@@ -1,11 +1,9 @@
 package main.java;
 
-import java.util.ArrayList;
-import java.util.List;
-import com.google.gson.*;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -15,13 +13,20 @@ public class IssueParser {
 public List<Issue> parseIssues(String s)
 {
     List<Issue> i=new ArrayList<Issue>();
-    Gson gson = new Gson();
+   /* Gson gson = new Gson();
     
        List<Issue> issues = new ArrayList<Issue>();
        Type collectionType = new TypeToken<List<Issue>>(){}.getType();
        i = gson.fromJson(s, collectionType);
 
-    
+   */
+    Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create();
+       
+       Type collectionType = new TypeToken<List<Issue>>(){}.getType();
+       i = gson.fromJson(s, collectionType);
+
   
     return i;
 }
